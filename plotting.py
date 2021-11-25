f210b8550738b4ff79fdd0d4d803290c4970f38e
[Minh <minhdangtn@tamu.edu>
2021-11-24 18:34:17 -0600

    update runtime plot

--- plotting.py
+++ plotting.py
@@ -1,31 +1,74 @@
+from collections import defaultdict
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-import sys, os
-
-args_len = 2
-
-if len(sys.argv) < args_len + 1:
-    print("require {} additional arguments".format(args_len-len(sys.argv)))
-    print("python3 ./plotting.py <heap_size> <benchmark>")
-    exit()
+import os
 
 
-heap_size = sys.argv[1]
-benchmarks = ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"]
-benchmark = benchmarks[int(sys.argv[2])]
-heuristics = ["qtable", "adaptive", "static", "compact", "aggressive"]
-file_names = [os.path.join("Shenandoah", h, heap_size, benchmark + ".csv") for h in heuristics]
+def plot_avg_runtime():
+    all_heap_sizes = ['128', '256', '512']
+    all_benchmarks = ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"]
+    heuristics = ["qtable", "adaptive", "static", "compact", "aggressive"]
 
-df = None
-for file_name, heuristic in zip(file_names, heuristics):
-    temp_df = pd.read_csv(file_name, sep=";", index_col="allocations", names=["allocations", heuristic], header=0)
-    if df is None:
-        df = temp_df
-        continue
-    df = pd.concat([df, temp_df], axis=1)
+    avg_runtime_df = pd.DataFrame({'benchmarks': ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"]})
+    # parse files
+    result_dict_128 = defaultdict(list)
+    result_dict_256 = defaultdict(list)
+    result_dict_512 = defaultdict(list)
+    for size in all_heap_sizes:
+        # calculate the avg runtime of each heuristic in each benchmark
+        for benchmark in all_benchmarks:
+            df = None
+            for h in heuristics:
+                file_name = (os.path.join("Shenandoah", h, size, benchmark + ".csv"))
+                temp_df = pd.read_csv(file_name, sep=";", index_col="allocations", names=["allocations", h], header=0)
+                df = pd.concat([df, temp_df], axis=1)
+            mean = df.mean().values.tolist()
+            for index, value in enumerate(mean):
+                if size == '128':
+                    result_dict_128[heuristics[index]].append(value)
+                elif size == '256':
+                    result_dict_256[heuristics[index]].append(value)
+                else:
+                    result_dict_512[heuristics[index]].append(value)
 
-print(df)
+    for key in result_dict_128.keys():
+        avg_runtime_df[key] = result_dict_128[key]
+    avg_runtime_df.plot.bar(rot=0)
+    figure = plt.gcf()
+    plt.ylabel('Application Runtime in Milliseconds')
+    plt.xlabel('Benchmarks')
+    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.title('Runtime Average of Application with heapsize of 128')
+    figure.set_size_inches(12, 9)
+    plt.savefig('plot/Runtime Average 128.png')
 
-df.plot(kind="line")
-plt.show()
+    for key in result_dict_256.keys():
+        avg_runtime_df[key] = result_dict_256[key]
+    avg_runtime_df.plot.bar(rot=0)
+    figure = plt.gcf()
+    plt.ylabel('Application Runtime in Milliseconds')
+    plt.xlabel('Benchmarks')
+    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.title('Runtime Average of Application with heapsize of 256')
+    figure.set_size_inches(12, 9)
+    plt.savefig('plot/Runtime Average 256.png')
+
+    for key in result_dict_512.keys():
+        avg_runtime_df[key] = result_dict_512[key]
+    avg_runtime_df.plot.bar(rot=0)
+    figure = plt.gcf()
+    plt.ylabel('Application Runtime in Milliseconds')
+    plt.xlabel('Benchmarks')
+    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.title('Runtime Average of Application with heapsize of 512')
+    figure.set_size_inches(12, 9)
+    plt.savefig('plot/Runtime Average 512.png')
+    # plt.show()
+
+
+def plot_mem_usage():
+    pass
+
+if __name__ == '__main__':
+    plot_avg_runtime()
