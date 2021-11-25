4916bc6f859c21139e5a7b3a977d08c25645db95
[Minh <minhdangtn@tamu.edu>
2021-11-25 14:20:20 -0600

    update plot for mem usage

--- plotting.py
+++ plotting.py
@@ -1,16 +1,17 @@
-from collections import defaultdict
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 import os
+import re
+from collections import defaultdict
+from matplotlib import pyplot as plt
 
 
 def plot_avg_runtime():
     all_heap_sizes = ['128', '256', '512']
-    all_benchmarks = ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"]
-    heuristics = ["qtable", "adaptive", "static", "compact", "aggressive"]
+    all_benchmarks = ['SingleCoreFixedSize', 'SingleCoreVaryingSize', 'MultipleCoresFixedSize', 'MultipleCoresVaryingSize']
+    heuristics = ['qtable', 'adaptive', 'static', 'compact', 'aggressive']
 
-    avg_runtime_df = pd.DataFrame({'benchmarks': ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"]})
+    avg_runtime_df = pd.DataFrame({'benchmarks': ['SingleCoreFixedSize', 'SingleCoreVaryingSize', 'MultipleCoresFixedSize', 'MultipleCoresVaryingSize']})
     # parse files
     result_dict_128 = defaultdict(list)
     result_dict_256 = defaultdict(list)
@@ -20,8 +21,8 @@ def plot_avg_runtime():
         for benchmark in all_benchmarks:
             df = None
             for h in heuristics:
-                file_name = (os.path.join("Shenandoah", h, size, benchmark + ".csv"))
-                temp_df = pd.read_csv(file_name, sep=";", index_col="allocations", names=["allocations", h], header=0)
+                file_name = (os.path.join('Shenandoah', h, size, benchmark + '.csv'))
+                temp_df = pd.read_csv(file_name, sep=';', index_col='allocations', names=['allocations', h], header=0)
                 df = pd.concat([df, temp_df], axis=1)
             mean = df.mean().values.tolist()
             for index, value in enumerate(mean):
@@ -38,7 +39,7 @@ def plot_avg_runtime():
     figure = plt.gcf()
     plt.ylabel('Application Runtime in Milliseconds')
     plt.xlabel('Benchmarks')
-    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.xticks(np.arange(len(all_benchmarks)), ['SingleCoreFixedSize', 'SingleCoreVaryingSize', 'MultipleCoresFixedSize', 'MultipleCoresVaryingSize'])
     plt.title('Runtime Average of Application with heapsize of 128')
     figure.set_size_inches(12, 9)
     plt.savefig('plot/Runtime Average 128.png')
@@ -49,7 +50,7 @@ def plot_avg_runtime():
     figure = plt.gcf()
     plt.ylabel('Application Runtime in Milliseconds')
     plt.xlabel('Benchmarks')
-    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.xticks(np.arange(len(all_benchmarks)), ['SingleCoreFixedSize', 'SingleCoreVaryingSize', 'MultipleCoresFixedSize', 'MultipleCoresVaryingSize'])
     plt.title('Runtime Average of Application with heapsize of 256')
     figure.set_size_inches(12, 9)
     plt.savefig('plot/Runtime Average 256.png')
@@ -60,7 +61,7 @@ def plot_avg_runtime():
     figure = plt.gcf()
     plt.ylabel('Application Runtime in Milliseconds')
     plt.xlabel('Benchmarks')
-    plt.xticks(np.arange(len(all_benchmarks)), ["SingleCoreFixedSize", "SingleCoreVaryingSize", "MultipleCoresFixedSize", "MultipleCoresVaryingSize"])
+    plt.xticks(np.arange(len(all_benchmarks)), ['SingleCoreFixedSize', 'SingleCoreVaryingSize', 'MultipleCoresFixedSize', 'MultipleCoresVaryingSize'])
     plt.title('Runtime Average of Application with heapsize of 512')
     figure.set_size_inches(12, 9)
     plt.savefig('plot/Runtime Average 512.png')
@@ -68,7 +69,33 @@ def plot_avg_runtime():
 
 
 def plot_mem_usage():
-    pass
+    pattern1 = re.compile('^capacity')
+    pattern2 = re.compile('^used')
+    heuristics = ['qtable', 'adaptive', 'static', 'compact', 'aggressive']
+    result = defaultdict(list)
+    for h in heuristics:
+        if h == 'qtable':
+            for file in os.listdir('log/'):
+                if file.startswith("gc_ad"):
+                    with open(os.path.join('log/', file), 'r') as f:
+                        mem = 0
+                        for line in f:
+                            line = line.rstrip()
+                            for match in re.finditer(pattern1, line):
+                                temp = line.split(' ')
+                                if temp[0] == '5':
+                                    mem = 512
+                                elif temp[0] == '2':
+                                    mem = 256
+                                else:
+                                    mem = 128
+
+                            for match in re.finditer(pattern2, line):
+                                temp = line.split(' ')
+                                result[mem].append(temp[1])
+    print(result)
 
 if __name__ == '__main__':
-    plot_avg_runtime()
+    # plot_avg_runtime()
+
+    plot_mem_usage()
